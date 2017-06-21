e1577b5eaac3c4446beba9ebfcbb57554c8a81b6
eina <eooka@teainc.org>
2017-06-21 15:03:13 -0700

    Update RPackages.r

--- RPackages.r
+++ RPackages.r
@@ -1,15 +1,35 @@
 
+## =========================================
+## Find Dependent Packages
+## =========================================
 
-install.packages(c("abind", "caret", "corrplot"
-                  , "devtools", "doParallel", "dplyr", "DT", "dygraphs"
-                  , "foreach", "forecast", "ggplot2", "jsonlite", "knitr"
-                  , "leaflet", "leaps",  "MASS", "moments", "mongolite"
-                   , "outliers"
-                  , "randomForest", "readxl",  "RColorBrewer", "RCurl", "reshape2", "rhandsontable", "RODBC"
-                  , "shiny", "shinydashboard", "shinyjs"
-                  , "timeDate", "zoo"))
-                  
-library(devtools)
-install_github("einaooka/tea.eo.plots")
-install_github("einaooka/tea.datetime")
-install_github("einaooka/tea.utilities")
+package.lst <- list("used" =c("abind", "caret", "forecast"))
+
+# Base packages
+package.lst$base = installed.packages()
+package.lst$base <- package.lst$base[which(package.lst$base[,"Priority"] == "base")]
+
+# Dependencies
+package.lst$depends <- package.lst$used
+
+temp <- unlist(lapply(package.lst$depends, tools::package_dependencies, available.packages()))
+temp <- unique(c(temp, package.lst$depends))
+print(length(temp))
+
+while (length(temp) != length(package.lst$depends)){
+  
+  package.lst$depends <- temp
+  temp <- unlist(lapply(package.lst$depends, tools::package_dependencies, installed.packages(), which = c("Depends", "Imports")))
+  temp <- unique(c(temp, package.lst$depends))
+  print(length(temp))
+}
+
+# Remove duplicates
+package.lst$depends <- package.lst$depends[!(package.lst$depends %in% package.lst$base)]
+package.lst$depends <- package.lst$depends[!(package.lst$depends %in% package.lst$used)]
+
+# Versions
+vers <- lapply(package.lst$depends, function(x)tryCatch(packageVersion(x), error = function(e) NA))
+names(vers) <- package.lst$depends
+vers
+
